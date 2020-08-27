a8cce09b630dc2da669579af59411f4cedab1b55
Joel Delatte <55522201+joeldelatte@users.noreply.github.com>
2020-08-27 10:47:29 -0500

    Update http-common.js

--- client/src/utils/http-common.js
+++ client/src/utils/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://solstice-challenge-joeldelatte.herokuapp.com/",
+  baseURL: "http://localhost:3001/",
   headers: {
     "Content-type": "application/json",
   },
